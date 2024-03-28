c8db544366de50854df001fa3867df1cd75a85eb
HostCase <430ap430@gmail.com>
2024-03-28 15:25:52 +0300

    z

--- Etu2sem1work.cpp
+++ Etu2sem1work.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
-
+#include "windows.h"
 using namespace std;
 typedef struct {
     int number_of_group;
@@ -22,31 +22,136 @@ typedef struct {
     grades_t grades;
 } student_t;
 
-void loadFromFile(student_t* students, int& ammountstudents, const string& filename) {
+void loadFromFile(student_t* students, int& ammountstudents, const string& studentsbase) {
+    ifstream file(studentsbase);
+    if (file.is_open()) {
+        file >> ammountstudents;
+        for (int studentcount = 0; studentcount < ammountstudents;studentcount+=1) {
+            file >> students[studentcount].name;
+            file >> students[studentcount].surname;
+            file >> students[studentcount].patronymic;
+            file >> students[studentcount].gender;
+            file >> students[studentcount].group.number_of_group;
+            file >> students[studentcount].group.id_of_student_in_group;
 
+            for (int i = 0; i < 3; ++i) {
+                file >> students[studentcount].grades.exam_grades[i];
+            }
+
+            for (int i = 0; i < 5; ++i) {
+                file >> students[studentcount].grades.credit_grades[i];
+            }
+
+        }
+
+        file.close();
+    }
+    else {
+        cerr << "Unable to open file for reading." << endl;
+    }
 }
 
-void addStudent(student_t* students, int &ammountstudents) {
+void addStudent(student_t* students, int& ammountstudents) {
 
+    cout << "Enter the name of the new student: ";
+    cin >> students[ammountstudents].name;
+
+    cout << "Enter the surname of the new student: ";
+    cin >> students[ammountstudents].surname;
+
+    cout << "Enter the patronymic of the new student: ";
+    cin >> students[ammountstudents].patronymic;
+
+    cout << "Enter the gender of the new student (M/F): ";
+    cin >> students[ammountstudents].gender;
+
+    cout << "Enter the group number of the new student: ";
+    cin >> students[ammountstudents].group.number_of_group;
+
+    cout << "Enter the ID in group of the new student: ";
+    cin >> students[ammountstudents].group.id_of_student_in_group;
+
+    cout << "Enter exam grades for the new student (3 grades): ";
+    for (int i = 0; i < 3; ++i) {
+        cin >> students[ammountstudents].grades.exam_grades[i];
+    }
+
+    cout << "Enter credit grades for the new student (5 grades): ";
+    for (int i = 0; i < 5; ++i) {
+        cin >> students[ammountstudents].grades.credit_grades[i];
+    }
+    ammountstudents += 1;
 }
-
 void editStudents(const student_t* students, int ammountstudents) {
 
 }
 
 void displayStudents(const student_t* students, int ammountstudents) {
+    
 
+        for (int studentcount = 0; studentcount < ammountstudents; studentcount += 1) {
+            cout << "Name: " << students[studentcount].name << endl;
+            cout << "Surname: " << students[studentcount].surname << endl;
+            cout << "Patronymic: " << students[studentcount].patronymic << endl;
+            cout << "Gender: " << students[studentcount].gender << endl;
+            cout << "Group number: " << students[studentcount].group.number_of_group << endl;
+            cout << "ID in group: " << students[studentcount].group.id_of_student_in_group << endl;
+
+
+
+        cout << "Exam grades: ";
+        for (int i = 0; i < 3; ++i) {
+            cout << students[studentcount].grades.exam_grades[i] << " ";
+        }
+        cout << endl;
+
+
+        cout << "Credit grades: ";
+        for (int i = 0; i < 5; ++i) {
+            cout << students[studentcount].grades.credit_grades[i]<<" ";
+        }
+        cout << endl;
+
+        cout << "-" << endl;
+        Sleep(150);
+    }
 }
 
-void saveToFile(const student_t* students, int ammountstudents, const string& filename) {
+void saveToFile(const student_t* students, int ammountstudents, const string& studentsbase) {
+    ofstream file;
+    file.open(studentsbase, ios::out);
+    if (file.is_open()) {
+        file << ammountstudents<< "\n";
+        for (int studentcount = 0; studentcount < ammountstudents; studentcount += 1) {
+            file << students[studentcount].name << "\n";
+            file << students[studentcount].surname << "\n";
+            file << students[studentcount].patronymic << "\n";
+            file << students[studentcount].gender << "\n";
+            file << students[studentcount].group.number_of_group << "\n";
+            file << students[studentcount].group.id_of_student_in_group << "\n";
 
+            for (int i = 0; i < 3; ++i) {
+                file << students[studentcount].grades.exam_grades[i] << "\n";
+            }
+
+            for (int i = 0; i < 5; ++i) {
+                file << students[studentcount].grades.credit_grades[i] << "\n";
+            }
+
+        }
+
+        file.close();
+    }
+    else {
+        cerr << "Unable to open file for reading." << endl;
+    }
 }
 
 
 int main() {
     int ammountstudents = 0;
-    student_t *students = new student_t[ammountstudents];
-    loadFromFile(students, ammountstudents, "students_data.txt");
+    student_t *students = new student_t[100000];
+    loadFromFile(students, ammountstudents, "studentsbase.txt");
     if (ammountstudents == 0) {
         addStudent(students, ammountstudents);
     }
@@ -70,9 +175,9 @@ int main() {
             displayStudents(students, ammountstudents);
             break;
         case 4:
-            saveToFile(students, ammountstudents, "students_data.txt");
+            saveToFile(students, ammountstudents, "studentsbase.txt");
             cout << "Exiting program." << endl;
-            break;
+            break; 
         default:
             cout << "Invalid choice. Please try again." << endl;
         }
